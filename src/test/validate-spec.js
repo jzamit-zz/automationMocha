432b1be0999b647ffaa905a91fdb6b444563be75
Jorge Zamit <jzamit@gmail.com>
2017-08-24 14:09:52 -0300

    Configuration changes

--- src/test/validate-spec.js
+++ src/test/validate-spec.js
@@ -1,7 +1,9 @@
-const assert = require('assert');
+const assert = require("assert");
+const describe = require("mocha").describe;
+const it = require("mocha").it;
 
-    describe('Be welcome to Mocha', function() {
-        it('with a failing test', function() {
-             assert(false, 'Hello World');
-     });
+describe("Be welcome to Mocha", () => {
+  it("with a failing test", () => {
+    assert(false, "Hello World");
+  });
 });
