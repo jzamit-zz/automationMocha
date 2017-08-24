9f5c2f846f89bca56aac35496d6003b767877161
Jorge Zamit <jzamit@gmail.com>
2017-08-24 12:10:06 -0300

    First working mocha example

---
+++ src/test/validate-spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+    describe('Be welcome to Mocha', function() {
+        it('with a failing test', function() {
+             assert(false, 'Hello World');
+     });
+});
